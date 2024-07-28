34dfda23e7504b6eca7fb1ff8eac4977cf800473
edemach <edemarchmsds@gmail.com>
2024-07-28 19:17:53 -0300

    Metodos e metodos contrutores

--- Cursos/Java/Java Básico/java-anatomia-classe/src/MinhaClasse.java
+++ Cursos/Java/Java Básico/java-anatomia-classe/src/MinhaClasse.java
@@ -1,10 +1,19 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-       int ano_2000 = 2000;
-       String nome="";
-       int numero;
-       double cpf;
+       
+       String nome          = "Edemarch";
+       String sobrenome     = "Moireira da Silva";
+      
+       String nomeCompleto = nomeCompleto(nome, sobrenome);
 
+       System.out.println(nomeCompleto);
+
+
+    }
+
+    public static String nomeCompleto(String nome,String sobrenome)
+    {
+        return nome.concat(" ").concat(sobrenome);
     }
     
 }
